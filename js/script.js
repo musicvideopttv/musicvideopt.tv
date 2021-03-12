f17383b46a67052a1aced1d9daf758065d8d0d56
musicvideopttv <80509992+musicvideopttv@users.noreply.github.com>
2021-03-12 10:36:36 +0000

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1135},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-social","displayName":"Social","link":{"linkType":"LinkTypePage","href":"#!page-social"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
 <script>
 
 // Function to reveal lightbox and adding YouTube autoplay
